e400f86f86a6a9388396ffeddbb6a726ed45c818
Skandan <54405114+skandanramesh@users.noreply.github.com>
2022-09-08 23:37:38 +0530

    Install NS3 version 3.36.1

---
+++ NetworkSimulator/installation.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+sudo apt install g++ python3 cmake 
+sudo apt install python3-dev pkg-config sqlite3 
+sudo apt install python3-setuptools git 
+sudo apt install qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
