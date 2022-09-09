c883aad23329274a92b9b53a6b1d31c3ed4975a4
Skandan <54405114+skandanramesh@users.noreply.github.com>
2022-09-09 09:40:55 +0530

    Update installation.sh

--- NetworkSimulator/installation.sh
+++ NetworkSimulator/installation.sh
@@ -7,7 +7,7 @@ sudo apt install python3-dev pkg-config sqlite3 python3-setuptools
 sudo apt install qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
 
 wget http://www.nsnam.org/release/ns-allinone-3.36.1.tar.bz2
-tar xjf ns-allinone-3.30.tar.bz2
+tar xjf ns-allinone-3.36.1.tar.bz2
 
 cd ns-allinone-3.36.1
 ./build.py
