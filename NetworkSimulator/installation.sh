871262b8e176856438515641de80989bd9754901
Skandan <54405114+skandanramesh@users.noreply.github.com>
2022-09-09 07:52:42 +0530

    Update installation.sh

--- NetworkSimulator/installation.sh
+++ NetworkSimulator/installation.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
+sudo apt update
+
 sudo apt install g++ python3 cmake 
-sudo apt install python3-dev pkg-config sqlite3 
-sudo apt install python3-setuptools git 
+sudo apt install python3-dev pkg-config sqlite3 python3-setuptools
 sudo apt install qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
 
 wget http://www.nsnam.org/release/ns-allinone-3.36.1.tar.bz2
@@ -12,3 +13,9 @@ cd ns-allinone-3.36.1
 ./build.py
 
 cd ns-3.36.1
+./ns3 configure --enable-tests --enable-examples -d optimized
+./test.py
+
+./ns3 configure --build-profile=debug --enable-examples --enable-tests
+./ns3
+./ns3 run hello-simulator
